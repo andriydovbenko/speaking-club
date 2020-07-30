04e28614d3f5266fd758f84a26ba2d0047b4eea4
andriydovbenko <andriydovbenko1@gmail.com>
2020-07-30 14:43:05 +0200

    init empty spring boot project

---
+++ src/main/java/com/speakfun/speakingclub/SpeakingClubApplication.java
@@ -0,0 +1,13 @@
+package com.speakfun.speakingclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakingClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakingClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speakfun/speakingclub/SpeakingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakfun.speakingclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
